e25d549b89e3e1064046f5ff56d4fa5fd0d9c6d2
Afoggs <fireheartfoggs@gmail.com>
2019-04-13 23:05:04 -0500

    Added jobs for easy pull.

---
+++ Week-09/maxtemp15.java
@@ -0,0 +1,43 @@
+// cc MaxTemperature Application to find the maximum temperature in the weather dataset
+// vv MaxTemperature
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
+
+public class MaxTemperature {
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperature <input path> <output path>");
+      System.exit(-1);
+    }
+
+    Job job = new Job();
+    job.setJarByClass(MaxTemperature.class);
+    job.setJobName("foggs-max-temperature-15");
+    job.setNumReduceTasks(4);
+
+    //Gzip Compression
+    job.getConfiguration().setBoolean("mapreduce.map.output.compress",true);
+    job.getConfiguration().setClass("mapreduce.map.output.compress.codec",GzipCodec.class, CompressionCodec.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setReducerClass(MaxTemperatureReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
+// ^^ MaxTemperature
---
+++ Week-09/maxtemp5.java
@@ -0,0 +1,43 @@
+// cc MaxTemperature Application to find the maximum temperature in the weather dataset
+// vv MaxTemperature
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
+
+public class MaxTemperature {
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      System.err.println("Usage: MaxTemperature <input path> <output path>");
+      System.exit(-1);
+    }
+
+    Job job = new Job();
+    job.setJarByClass(MaxTemperature.class);
+    job.setJobName("foggs-max-temperature-5");
+
+    //Snappy Compression
+    job.getConfiguration().setBoolean("mapreduce.map.output.compress",true);
+    job.getConfiguration().setClass("mapreduce.map.output.compress.codec",SnappyCodec.class, CompressionCodec.class);
+
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.setMapperClass(MaxTemperatureMapper.class);
+    job.setReducerClass(MaxTemperatureReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
+// ^^ MaxTemperature
